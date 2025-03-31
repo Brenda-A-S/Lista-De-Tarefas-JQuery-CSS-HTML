a33bac5442b4f04dd6654dcfa7b972e2fa104b92
Brenda Antunes <brenda.antunes16@gmail.com>
2025-03-31 19:13:32 -0300

    Add Local Storage

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
 $(document).ready(function () {
-    $('header button').click(function () {
-        let form = $('form');
-        if (form.is(':visible')) {
-            form.slideUp();
-        } else {
-            form.slideDown();
+    const localStorageKey = "listaTarefas";
+
+    function salvarTarefas() {
+        let tarefas = [];
+        $("ul li").each(function () {
+            let texto = $(this).text();
+            let concluida = $(this).hasClass("checked");
+            tarefas.push({ texto, concluida });
+        });
+        localStorage.setItem(localStorageKey, JSON.stringify(tarefas));
+    }
+    function carregarTarefas() {
+        let tarefasSalvas = localStorage.getItem(localStorageKey);
+        if (tarefasSalvas) {
+            tarefasSalvas = JSON.parse(tarefasSalvas);
+            tarefasSalvas.forEach(tarefa => {
+                adicionarTarefaNaLista(tarefa.texto, tarefa.concluida);
+            });
         }
-    });
-    $('form').on('submit', function (e) {
-        e.preventDefault();
-        const nomeNovaTarefa = $('#nome-tarefa').val();
-        const novoItem = $(`<li style="display:none">${nomeNovaTarefa}</li>`);
+    }
+    function adicionarTarefaNaLista(nome, concluida = false) {
+        const novoItem = $(`<li style="display:none">${nome}</li>`);
         $('<button class="excluir"></button>').appendTo(novoItem);
-        novoItem.appendTo('ul');
-        novoItem.fadeIn();
-        $('#nome-tarefa').val('');
+        if (concluida) novoItem.addClass("checked");
+        novoItem.appendTo("ul").fadeIn();
+    }
+    $("header button").click(function () {
+        let form = $("form");
+        form.is(":visible") ? form.slideUp() : form.slideDown();
     });
-    $('ul').on('click', 'li', function () {
-        $(this).toggleClass('checked');
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+        const nomeNovaTarefa = $("#nome-tarefa").val().trim();
+        if (nomeNovaTarefa !== "") {
+            adicionarTarefaNaLista(nomeNovaTarefa);
+            salvarTarefas();
+        }
+        $("#nome-tarefa").val("");
     });
-    $('ul').on('click', 'li button', function (e) {
+    $("ul").on("click", "li", function () {
+        $(this).toggleClass("checked");
+        salvarTarefas();
+    });
+    $("ul").on("click", "li button", function (e) {
         e.stopPropagation();
-        const item = $(this).parent('li');
-        item.fadeOut();
-        setTimeout(function () {
+        const item = $(this).parent("li");
+        item.fadeOut(() => {
             item.remove();
-        }, 1000);
+            salvarTarefas();
+        });
     });
+    carregarTarefas();
 });
